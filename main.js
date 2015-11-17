384d79d5b1311d60b537aee7767a7c2e6b46cdc4
严春光 <842997290@qq.com>
2015-11-17 23:28:53 +0800

    native平台处理

--- main.js
+++ main.js
@@ -2,5 +2,28 @@
  * Created by yanchunguang on 15/10/27.
  */
 
+if (cc.sys.isNative)
+{
+    cc.game.onStart = function()
+    {
+        var resolutionPolicy = (cc.sys.os == cc.sys.OS_WP8 || cc.sys.os == cc.sys.OS_WINRT) ? cc.ResolutionPolicy.SHOW_ALL : cc.ResolutionPolicy.FIXED_HEIGHT;
+        cc.view.setDesignResolutionSize(800, 450, resolutionPolicy);
+        cc.view.resizeWithBrowserSize(true);
 
-cc.director.runScene(new GScene());
+        var searchPaths = jsb.fileUtils.getSearchPaths();
+        searchPaths.push('script');
+        searchPaths.push('src');
+        jsb.fileUtils.setSearchPaths(searchPaths);
+
+        require("includeFile.js");
+
+        cc.director.runScene( new GScene());
+    };
+    cc.game.run();
+}
+else
+{
+    cc.director.runScene(new GScene());
+}
+
+
